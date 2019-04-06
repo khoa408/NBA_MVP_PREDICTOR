fc538a028d544a045f5ab8e7c2624c75dd382a58
Khoa Tran <khoa.d.tran97@gmail.com>
2019-04-05 17:22:20 -0700

    initial commit

---
+++ NBA_LR.py
@@ -0,0 +1,86 @@
+import csv
+import pandas as pd
+import numpy as np
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+
+# CSV path
+CSV_PATH = "/Users/khoatran/Desktop/NBA_Data.csv"
+
+# malignant: 1 (239)
+# benign   : 0 (444)
+
+def main():
+    # Load Data
+    dataset = pd.read_csv(CSV_PATH)
+    print "Observation Count: ", len(dataset)
+
+    # Print top 10 data using pandas lib
+    # to test if data has been loaded successfully
+    print dataset.head(10)
+
+    # Extract column names
+    column_names = list(dataset.columns.values)
+    print "Dataset Column Names: {column_names}".format(column_names=column_names)
+
+    # Selecting features
+    # Not including ...
+
+    features = ['MIN','PTS','FG_PCT','FG3_PCT','FT_PCT','REB','AST',\
+        'STL','BLK','TOV','PER','OFFRTG','DEFRTG','NETRTG','AST_PCT',\
+        'AST_TO_TO','AST_RATIO','TO_RATIO','EFG','TS',\
+        'USG_RATE','WIN_SHARE']
+    target = 'PTS_WON'
+    # splitting training and testing data (70:30)
+    train_x, test_x, train_y, test_y = train_test_split(dataset[features], dataset[target], train_size=0.7)
+    
+    print "train_x size: ", len(train_x)
+    print "train_y size: ", len(train_y)
+ 
+    print "test_x size: ", len(test_x)
+    print "test_y size: ", len(test_y)
+    
+    # Upsample training data
+    # Fix data imbalace using SMOTE
+    # smote = SMOTE()
+    # train_x_smote, train_y_smote = smote.fit_sample(train_x, train_y)
+    # print "SMOTE has been applied to balance the training data"
+
+	# Train Logistic Regression
+    # Limited-memory Browden-Fletcher-Goldfarb-Shanno
+    LR_model = LinearRegression()
+    LR_model.fit(train_x,train_y)
+
+    # Calculate Accuracy
+    LR_accuracy_train = LR_model.score(train_x, train_y)
+    LR_accuracy_test  = LR_model.score(test_x, test_y)
+    print "Train Accuracy: ", LR_accuracy_train
+    print "Test Accuracy : ", LR_accuracy_test
+
+    # Make predictions using the testing set
+    y_pred = LR_model.predict(test_x)
+
+    # The coefficients
+    print('Coefficients: \n', LR_model.coef_)
+    # The mean squared error
+    print("Mean squared error: %.2f"
+          % mean_squared_error(test_y, y_pred))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.2f' % r2_score(test_y, y_pred))
+
+    # Plot outputs
+    plt.scatter(test_x, test_y,  color='black')
+    plt.plot(test_x, y_pred, color='blue', linewidth=3)
+
+    plt.xticks(())
+    plt.yticks(())
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
